06c34aa47657a2365c7d983bc075a21893485f22
codebudy5247 <a.shekhar5247@gmail.com>
2020-12-01 23:17:38 +0530

    push to heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const dotenv =  require('dotenv').config();
 
 // init app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // register view engine
 const hbs = exphbs.create({
